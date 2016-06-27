d65085347dde0baf9f9d7d100e92b90973e85ae8
nezumin <nezumin@chillamins-cafe.jp>
2016-06-28 00:51:22 +0900

    初回コミット

---
+++ HardwareProfile.h
@@ -0,0 +1,5 @@
+#define HARDWARE_PROFILE_H
+//#define DEMO_BOARD USER_DEFINED_BOARD
+
+#define USB_BUS_SENSE 1
+#define self_power 1
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=jubeat_pad.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/jubeat_pad.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=jubeatpad.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/jubeatpad.x.tar
